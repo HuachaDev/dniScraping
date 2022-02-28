8834c082fba4228c7a616e33c5e920bf58bddb68
HuachaDev <luismickey2198@gmail.com>
2022-02-27 23:13:35 -0500

    Creando servidor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-const express = require
+const express = require('express');
+const morgan = require('morgan');
+const { database }  = require('./keys');
+
+const app = express();
+
+app.set('port' , process.env.port || 4000 );
+
+//app.use(morgan('dev'));
+
+app.use(require('./routes/index'))
+
+app.listen(app.get('port') , () => {
+    console.log('El servidor esta en el puerto', app.get('port'));
+})
